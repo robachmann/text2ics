95e4391be2afd0980b9727396d9d01a737e33057
Roman Bachmann <git@240.ch>
2025-10-25 22:51:22 +0200

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        teal: {
+          50: '#f2fdfc',
+          100: '#e6fbf9',
+          200: '#bfefea',
+          300: '#99e3db',
+          400: '#4fd1c2',
+          500: '#16a39a',
+          600: '#12857f',
+          700: '#0f665f',
+          800: '#0b483f',
+          900: '#06261f'
+        }
+      }
+    }
+  },
+  plugins: [],
+}
---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "/text2ics/",
+  plugins: [react()],
+});
